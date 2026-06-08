ce63a2d8da3cc3fb84806ce53041956f8c61c9c6
Cyrus <mrosyrus@gmail.com>
2026-06-08 18:15:19 +0000

    🍝 Recipe swap: −Hot Italian Sausage Spaghetti & old Squash Ribbon Flatbread; salad now permanent; +Sausage & Squash Flatbread (lunch) + Pasta (dinner) queued today w/ shared prep; 🍯 Hot honey → grocery; 🥚 Eggs back in-stock. SW hg-v46.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hg-v45';
+const CACHE='hg-v46';
 const SHELL=['./','./index.html','./data.js','./recipes.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
